d4b601a6f8e02b9f5695d874aece6579c87cdb6d
ElizabethHolgate <44809627+ElizabethHolgate@users.noreply.github.com>
2023-09-26 12:58:50 +0100

    attempt to fix github actions error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+  /* config options here */
+}
+ 
 module.exports = nextConfig
